6e255d27885df84bb631ae0deb3f7f0cbebb286e
Yoimer <ydrf19@gmail.com>
2017-10-15 18:08:20 -0400

    commented debugging lines on initialize.php

--- globe_bank/private/initialize.php
+++ globe_bank/private/initialize.php
@@ -4,13 +4,13 @@
 	// __FILE__ returns the current path to this file
 	// dirname() returns the path to the parent directory
 	define("PRIVATE_PATH", dirname(__FILE__));
-	echo PRIVATE_PATH . "<br />";
+	//echo PRIVATE_PATH . "<br />";
 	define("PROJECT_PATH", dirname(PRIVATE_PATH));
-	echo PROJECT_PATH . "<br />";
+	//echo PROJECT_PATH . "<br />";
 	define("PUBLIC_PATH", PROJECT_PATH . '/public');
-	echo PUBLIC_PATH . "<br />";
+	//echo PUBLIC_PATH . "<br />";
 	define("SHARED_PATH", PRIVATE_PATH . '/shared');
-	echo SHARED_PATH . "<br />";
+	//echo SHARED_PATH . "<br />";
 
 	// Assign the root URL to a PHP constant
 	// * Do not need to include the domain
@@ -20,11 +20,11 @@
 	// define("WWW_ROOT", '');
 	// * Can dynamically find everything in URL up to "/public"
 	$public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
-	echo $_SERVER['SCRIPT_NAME'] . "<br />";
-	echo strpos($_SERVER['SCRIPT_NAME'], '/public')  . "<br />";
+	//echo $_SERVER['SCRIPT_NAME'] . "<br />";
+	//echo strpos($_SERVER['SCRIPT_NAME'], '/public')  . "<br />";
 	$doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 	define("WWW_ROOT", $doc_root); // makes WWW_ROOT an absolute value that we can be used in our web pages
-	echo $doc_root . "<br/>";
+	//echo $doc_root . "<br/>";
 
 	require_once('functions.php');
 	/*echo PRIVATE_PATH . "<br />";
