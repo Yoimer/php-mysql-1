c9c32086d387554c6e8721aed7ff9a81de113845
Yoimer <ydrf19@gmail.com>
2017-10-16 17:24:55 -0400

    default-values-for-url-parameters

--- globe_bank/public/staff/subjects/show.php
+++ globe_bank/public/staff/subjects/show.php
@@ -1,4 +1,23 @@
 <?php
-	$id = $_GET['id'];
-	echo $id;
+	/*$id = $_GET['id'];
+	echo $id;*/
+
+//Default values
+
+/*$page = $_GET['page'];
+
+if (isset($_GET['page]))
+{
+	$page = $_GET['page'];
+}
+else
+{
+	$page = '1';
+}*/
+
+//$page = $_GET['page'] ?? '1'; // PHP > 7.0
+
+$id = isset($_GET['id']) ? $_GET['id'] : 'DEFAULT VALUE';
+echo $id . "<br />";
+
 ?>
