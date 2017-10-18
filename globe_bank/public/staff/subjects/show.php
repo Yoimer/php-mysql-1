24b0a6660fc302cba75b2e80fa2dca32820be2ca
Yoimer <ydrf19@gmail.com>
2017-10-18 14:11:44 -0400

    encode-url-parameters

--- globe_bank/public/staff/subjects/show.php
+++ globe_bank/public/staff/subjects/show.php
@@ -1,39 +1,12 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
 <?php
-	/*$id = $_GET['id'];
-	echo $id;*/
 
-//Default values
-
-/*$page = $_GET['page'];
-
-if (isset($_GET['page]))
-{
-	$page = $_GET['page'];
-}
-else
-{
-	$page = '1';
-}*/
-
-//$page = $_GET['page'] ?? '1'; // PHP > 7.0
-
-//$id = isset($_GET['id']) ? $_GET['id'] : 'DEFAULT VALUE';
-//echo $id . "<br />";
-//echo $id;
-
-//$page = isset($_GET['page']) ? $_GET['page'] : 'DEFAULT VALUE';
-//echo $page . "<br />";
-//echo $page;
-
-$page2 = isset($_GET['page2']) ? $_GET['page2'] : 'DEFAULT VALUE';
-//echo $page2 . "<br />";
-echo $page2;
-
-$comp = (int)$page2;
-
-if ($comp == 32)
-{
-	echo "value of. " . $page2;
-}
+$id = isset($_GET['id']) ? $_GET['id'] : 'DEFAULT VALUE';
+echo $id . "<br />";
 
 ?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br />
