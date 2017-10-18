93abb2b2bbfbdc59b5cad37f448c86e4061e4405
Yoimer <ydrf19@gmail.com>
2017-10-18 00:10:38 -0400

    modified show.php for showing data from nodemcu

--- globe_bank/public/staff/subjects/show.php
+++ globe_bank/public/staff/subjects/show.php
@@ -17,14 +17,23 @@ else
 
 //$page = $_GET['page'] ?? '1'; // PHP > 7.0
 
-$id = isset($_GET['id']) ? $_GET['id'] : 'DEFAULT VALUE';
-echo $id . "<br />";
+//$id = isset($_GET['id']) ? $_GET['id'] : 'DEFAULT VALUE';
+//echo $id . "<br />";
+//echo $id;
 
-$page = isset($_GET['page']) ? $_GET['page'] : 'DEFAULT VALUE';
-echo $page . "<br />";
+//$page = isset($_GET['page']) ? $_GET['page'] : 'DEFAULT VALUE';
+//echo $page . "<br />";
+//echo $page;
 
 $page2 = isset($_GET['page2']) ? $_GET['page2'] : 'DEFAULT VALUE';
-echo $page2 . "<br />";
+//echo $page2 . "<br />";
+echo $page2;
 
+$comp = (int)$page2;
+
+if ($comp == 32)
+{
+	echo "value of. " . $page2;
+}
 
 ?>
