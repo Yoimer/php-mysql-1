4e022827b130ae5ca94b6e7fd95e51bfb5df5fbe
Yoimer <ydrf19@gmail.com>
2017-10-21 01:37:59 -0400

    modify-headers

---
+++ globe_bank/public/staff/subjects/new.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('../../../private/initialize.php');
+
+//$test = $_GET['test'] ?? '';
+
+$test = isset($_GET['test']) ? $_GET['test'] : 'DEFAULT VALUE';
+
+if ($test == '404') {
+	error_404();
+}	elseif($test == '500') {
+	error_500();
+}	else {
+	echo 'No error';
+}
+
+?>
