f86469b2ecfb36caa9b5156c03c175dd3ccc3813
Yoimer <ydrf19@gmail.com>
2017-11-04 21:39:59 -0400

    error-handling

--- globe_bank/private/query_functions.php
+++ globe_bank/private/query_functions.php
@@ -2,9 +2,12 @@
 
     function find_all_subjects() {
         global $db;
+
         $sql = "SELECT * FROM subjects ";
         $sql .= "ORDER BY position ASC";
+        //echo $sql;
         $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
         return $result;
     }
 
